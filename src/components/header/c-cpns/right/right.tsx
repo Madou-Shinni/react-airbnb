094d9a753334080e8f2160bdcd576f87bce55c5e
xw <xw188@yum.com>
2022-11-22 17:21:25 +0800

    【add】1.profile面板点击切换效果

--- src/components/header/c-cpns/right/right.tsx
+++ src/components/header/c-cpns/right/right.tsx
@@ -1,10 +1,30 @@
-import React from "react"
+import React, {useEffect, useState} from "react"
 import {RightWrapper} from "./style"
 import IconGlobal from "@/assets/svg/icon_global";
 import IconMenu from "@/assets/svg/icon_menu";
 import IconAvatar from "@/assets/svg/IconAvatar";
 
 const Right: React.FC = () => {
+  const [showPanel,setShowPanel] = useState(false)
+
+  function profileHandleClick() {
+    setShowPanel(true)
+  }
+
+  /* 副作用hooks */
+  useEffect(()=>{
+    function windowsHandleClick() {
+      setShowPanel(false)
+    }
+    /* 监听窗口点击 true:设置捕获 */
+    window.addEventListener("click",windowsHandleClick,true)
+
+    /* 取消监听 */
+    return () => {
+      window.removeEventListener("click",windowsHandleClick,true)
+    }
+  },[])
+
   return <RightWrapper>
     <div className={"btns"}>
       <span className={"btn"}>登录</span>
@@ -14,9 +34,21 @@ const Right: React.FC = () => {
       </span>
     </div>
 
-    <div className={"profile"}>
+    <div className={"profile"} onClick={profileHandleClick}>
       <IconMenu />
       <IconAvatar />
+
+      { showPanel && (<div className={"panel"}>
+        <div className={"top"}>
+          <div className={"item register"}>注册</div>
+          <div className={"item login"}>登录</div>
+        </div>
+        <div className={"bottom"}>
+          <div className={"item"}>出租房源</div>
+          <div className={"item"}>开展体验</div>
+          <div className={"item"}>帮助</div>
+        </div>
+      </div>)}
     </div>
   </RightWrapper>;
 }
--- src/components/header/c-cpns/right/style.ts
+++ src/components/header/c-cpns/right/style.ts
@@ -7,27 +7,29 @@ export const RightWrapper = styled.div`
   justify-content: flex-end;
   /* 垂直方向居中 */
   align-items: center;
-  
+
   color: ${props => props.theme.text.color.primary};
   font-size: 14px;
   font-weight: 600;
-  
+
   .btns {
     display: flex;
-    
+
     .btn {
       height: 18px;
       line-height: 18px;
       padding: 12px 15px;
       cursor: pointer;
       border-radius: 22px;
+
       &:hover {
         background-color: #f5f5f5;
       }
     }
   }
-  
+
   .profile {
+    position: relative;
     display: flex;
     align-items: center;
     justify-content: space-evenly;
@@ -38,7 +40,38 @@ export const RightWrapper = styled.div`
     border-radius: 25px;
     background-color: #fff;
     cursor: pointer;
-    
+
     ${props => props.theme.mixin.boxShadow};
+
+    .panel {
+      position: absolute;
+      top: 60px;
+      right: 0;
+      width: 200px;
+      height: 230px;
+      background-color: #fff;
+      border-radius: 10px;
+      box-shadow: 0 0 2px 4px rgba(0, 0, 0, .18);
+      color: #666;
+
+      .top, .bottom {
+        padding: 10px 0;
+        text-align: left;
+
+        .item {
+          height: 40px;
+          line-height: 40px;
+          padding: 0 16px;
+
+          &:hover {
+            background-color: #f5f5f5;
+          }
+        }
+      }
+
+      .top {
+        border-bottom: 1px solid #ddd;
+      }
+    }
   }
 `
