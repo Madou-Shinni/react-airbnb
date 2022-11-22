5dc7bc762769b06b1f96c54829b812e2dae0fdd8
xw <xw188@yum.com>
2022-11-22 14:02:49 +0800

    【add】1.header footer组件化

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 import './App.css';
 import {useRoutes} from "react-router-dom";
 import routers from "@/router";
+import Footer from "@/components/footer/Footer";
+import Header from "@/components/header/Header";
 
 function App() {
   return (
     <div className="App">
-      <div className={"header"}>header</div>
+      <Header />
       <div className={"page"}>
         {useRoutes(routers)}
       </div>
-      <div className={"footer"}>footer</div>
+      <Footer />
     </div>
   );
 }
