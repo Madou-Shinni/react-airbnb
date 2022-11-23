f33e2f6de56770c730d49b574f32d303522c8095
xw <xw188@yum.com>
2022-11-23 14:26:20 +0800

    【add】1.首页-房间item布局展示

--- src/components/room-item/RoomItem.tsx
+++ src/components/room-item/RoomItem.tsx
@@ -4,13 +4,28 @@ import {RoomItemWrapper} from "./style"
 type Props = {
   itemData: {
     name: string
+    picture_url:string
+    verify_info:{
+      messages:[]
+      text_color:string
+    }
+    price: number
   }
 }
 
 const RoomItem: React.FC<Props> = (props) => {
   const { itemData } = props
-  return <RoomItemWrapper>
-    {itemData.name}
+  return <RoomItemWrapper verifyColor={itemData?.verify_info?.text_color || "#39576a"}>
+    <div className={"inner"}>
+      <div className={"cover"}>
+        <img src={itemData.picture_url} alt=""/>
+      </div>
+      <div className={"desc"}>
+        {itemData.verify_info.messages.join(".")}
+      </div>
+      <div className={"name"}>{itemData.name}</div>
+      <div className={"price"}>¥{itemData.price}</div>
+    </div>
   </RoomItemWrapper>;
 }
 
