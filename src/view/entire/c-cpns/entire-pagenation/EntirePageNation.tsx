9ed9ea11070fccc78b836e4d91c2c7547b11d24a
xw <xw188@yum.com>
2022-11-25 16:29:47 +0800

    【add】1.entire页码组件展示 2.样式重置修改ul li解决分页器无法显示

--- src/view/entire/c-cpns/entire-pagenation/EntirePageNation.tsx
+++ src/view/entire/c-cpns/entire-pagenation/EntirePageNation.tsx
@@ -1,10 +1,37 @@
 import React from "react"
+import Pagination from '@mui/material/Pagination';
+
 import { EntirePageNationWrapper } from "./style";
+import {useSelector} from "react-redux";
 
 type Props = {}
 
 const EntirePageNation: React.FC<Props> = (props) => {
-  return <EntirePageNationWrapper></EntirePageNationWrapper>;
+  const {totalCount,currentPage,roomList} = useSelector((state:any) => ({
+    totalCount: state.entire.totalCount,
+    currentPage: state.entire.currentPage,
+    roomList: state.entire.roomList
+  }))
+
+  const totalPage = Math.ceil(totalCount / 20)
+  const startCount = currentPage * 20 +1
+  const endCount = (currentPage + 1) * 20
+
+  return <EntirePageNationWrapper>
+    {/* !! 转为boolean */}
+    {
+      !!roomList.length &&
+      (
+        <div className={"info"}>
+          <Pagination count={totalPage}/>
+
+          <div className={"desc"}>
+            第 {startCount} – {endCount} 个房源，共超过 {totalCount} 个
+          </div>
+        </div>
+      )
+    }
+  </EntirePageNationWrapper>;
 }
 
 export default EntirePageNation;
--- src/view/entire/c-cpns/entire-pagenation/style.ts
+++ src/view/entire/c-cpns/entire-pagenation/style.ts
@@ -1,5 +1,28 @@
 import styled from "styled-components";
 
 export const EntirePageNationWrapper = styled.div`
+  display: flex;
+  justify-content: center;
   
+  .info {
+    .MuiPaginationItem-page:hover {
+      text-decoration: underline;
+    }
+    /* 覆盖mui样式,同时满足 */
+    .MuiPaginationItem-page.Mui-selected {
+      background-color: rgb(34, 34, 34);
+      color: #fff;
+    }
+    
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .desc {
+    margin-top: 10px;
+    font-weight: 400;
+    font-size: 14px;
+    line-height: 18px;
+  }
 `
