290b7afc0fba781d8f650d020638ca39a26d2f81
xw <xw188@yum.com>
2022-11-25 13:53:40 +0800

    【add】1.entire界面的filter展示

--- src/view/entire/c-cpns/entire-filter/EntireFilter.tsx
+++ src/view/entire/c-cpns/entire-filter/EntireFilter.tsx
@@ -1,10 +1,42 @@
-import React from "react"
+import React, {useState} from "react"
 import { EntireFilterWrapper } from "./style";
+import filterData from "@/assets/data/filter_data.json"
+import classNames from "classnames";
 
 type Props = {}
 
 const EntireFilter: React.FC<Props> = (props) => {
-  return <EntireFilterWrapper></EntireFilterWrapper>;
+  const [selectItems, setSelectItems] = useState<any>([])
+
+  const itemClickHandle = (item:any) => {
+    const newItems = [...selectItems]
+    if (newItems.includes(item)) {
+      // 如果当前选中项 已经被选中则移除
+      const itemIndex = newItems.findIndex(filterItem => filterItem === item)
+      newItems.splice(itemIndex,1)
+    }else {
+      // 没被选中 则加入
+      newItems.push(item)
+    }
+
+    setSelectItems(newItems)
+  }
+
+  return <EntireFilterWrapper>
+    <div className={"filter"}>
+      {
+        filterData.map((v,i) => (
+          <div
+            className={classNames("item",{active: selectItems.includes(v)})}
+            key={i}
+            onClick={e => itemClickHandle(v)}
+          >
+            {v}
+          </div>
+        ))
+      }
+    </div>
+  </EntireFilterWrapper>;
 }
 
 export default EntireFilter;
