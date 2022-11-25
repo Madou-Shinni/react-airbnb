66988dd0cc47557ae575d7ea6f88090c91707204
xw <xw188@yum.com>
2022-11-25 13:58:11 +0800

    【add】1.使用useNavigate，点击logo回到首页

--- src/components/header/c-cpns/left/left.tsx
+++ src/components/header/c-cpns/left/left.tsx
@@ -1,10 +1,16 @@
 import React from "react"
 import {LeftWrapper} from "./style"
 import IconLogo from "@/assets/svg/icon_logo";
+import {useNavigate} from "react-router-dom";
 
 const Left: React.FC = () => {
+  const navigate = useNavigate()
+  const logoClickHandle = () => {
+    navigate("/home")
+  }
+
   return <LeftWrapper>
-    <div className={"logo"}>
+    <div className={"logo"} onClick={logoClickHandle}>
       <IconLogo />
     </div>
   </LeftWrapper>;
